65a92d44914a2f342e7ca04764e100a41f88b752
emanuel-inteli <82065728+emanuelcop3@users.noreply.github.com>
2022-06-18 21:55:13 -0300

    semana 7

--- 03_AUT_EST_ENTREGA/Semana 7/script.js
+++ 03_AUT_EST_ENTREGA/Semana 7/script.js
@@ -1,14 +1,49 @@
+function myFunction() {
+  document.getElementById("field2").value = document.getElementById("field1").value;
+}
+
+
+$(document).ready(function(){
+    curiculo.list("2")
+})
+var  curiculo = {
+    list(Id){
+            $.ajax({
+                type:'GET',
+                url:  'http://localhost:127.0.0.1:5500/' + Id,
+                success: function(resultados){
+           //         document.getElementById("questionsHeader").textContent = resultados.user.nome
+           resultados.forEach(questao => {
+var element = `<p><a href="https://github.com/emanuelcop3">Acesse meu perfil no GitHub</a></p>`
+document.getElementById("list").innerHTML += element
+
+        });
+                }
+            })
+    }
+}
+
+
+
+
+
+
+
+
 const express = require('express'); 
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const hostname = '127.0.0.1';
+
+onclick="document.getElementById('demo').innerHTML = Date()"
+
 
 const port = 3061;
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const DBPATH = 'dbUser.db';
 
-app.use(express.static("../frontend/"));
+app.use(express.static("../Semana /"));
 
 app.use(express.json());
 
