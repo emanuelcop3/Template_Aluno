6027078812064a5f4e17d75ef303a0a93eabc542
emanuel-inteli <82065728+emanuelcop3@users.noreply.github.com>
2022-06-18 21:51:43 -0300

    semana 6
    semana 6 update

--- 03_AUT_EST_ENTREGA/Semana 6/script.js
+++ 03_AUT_EST_ENTREGA/Semana 6/script.js
@@ -1,27 +1,48 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const hostname = '127.0.0.1';
+
+onclick="document.getElementById('demo').innerHTML = Date()"
+
+
+const port = 3061;
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const DBPATH = 'dbUser.db';
+
+app.use(express.static("../Semana 6/"));
+
+app.use(express.json());
+
+
+/* Definição dos endpoints */
+
+/****** CRUD ******************************************************************/
+
 function myFunction() {
-  document.getElementById("field2").value = document.getElementById("field1").value;
-}
-
-
-
-
-
-$(document).ready(function(){
-    curiculo.list("2")
-})
-var  curiculo = {
-    list(eixoId){
-            $.ajax({
-                type:'GET',
-                url:  'http://localhost:127.0.0.1:5500/' + eixoId,
-                success: function(resultados){
-           //         document.getElementById("questionsHeader").textContent = resultados.user.nome
-           resultados.questoes.forEach(questao => {
-var element = `<p><a href="https://github.com/emanuelcop3">Acesse meu perfil no GitHub</a></p>`
-document.getElementById("list").innerHTML += element
-
-        });
-                }
-            })
-    }
-}
+	document.getElementById("field2").value = document.getElementById("field1").value;
+  }
+  
+  
+  $(document).ready(function(){
+	  curiculo.list("2")
+  })
+  var  curiculo = {
+	  list(Id){
+			  $.ajax({
+				  type:'GET',
+				  url:  'http://localhost:127.0.0.1:5500/' + Id,
+				  success: function(resultados){
+			 //         document.getElementById("questionsHeader").textContent = resultados.user.nome
+			 resultados.forEach(questao => {
+  var element = `<p><a href="https://github.com/emanuelcop3">Acesse meu perfil no GitHub</a></p>`
+  document.getElementById("list").innerHTML += element
+  
+		  });
+				  }
+			  })
+	  }
+  }
+  
+  
